4b9846eec8e1a955f7ba78d47e8c33d78f993c12
pipiraji <pipiraji@gmail.com>
2025-03-27 20:59:01 +0900

    mmmm

--- test/test_say.py
+++ test/test_say.py
@@ -1,8 +1,9 @@
 import sys
 from os import path
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
     from FirstPackage.Say import *
+
     Say().print_say()
